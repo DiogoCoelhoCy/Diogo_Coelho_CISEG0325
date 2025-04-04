3de39887e4723b1194946c46d1aadc49cd9b1559
Diogo Coelho <diogo.coelho.t0130710@edu.atec.pt>
2025-04-04 20:05:47 +0100

    update 2.0

---
+++ Diogo_Coelho_CISEG0325/Aula 04-04/exercises/matchcaseEX.py
@@ -0,0 +1,32 @@
+
+print("Bem vindo")
+
+mes=int(input("Digite um numero (1 a 12): "))
+
+match mes:
+    case 1:
+        print("janeiro")
+    case 2:
+        print("fevereiro")
+    case 3:
+        print("Março")
+    case 4:
+        print("Abril")
+    case 5:
+        print("Maio")
+    case 6:
+        print("Junho")
+    case 7:
+        print("Julho")
+    case 8:
+        print("Agosto")
+    case 9:
+        print("Setembro")
+    case 10:
+        print("Outubro")
+    case 11:
+        print("Novembro")
+    case 12:
+        print("Dezembro")
+    case _:
+        print("Invalido")
