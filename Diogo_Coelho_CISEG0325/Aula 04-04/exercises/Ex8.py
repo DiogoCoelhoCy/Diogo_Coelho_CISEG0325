0a0e0755d40e4d4df7723f3fec8c148facaeeff1
Diogo Coelho <diogo.coelho.t0130710@edu.atec.pt>
2025-04-04 19:52:40 +0100

    update 1.0

--- Diogo_Coelho_CISEG0325/Aula 04-04/exercises/Ex8.py
+++ Diogo_Coelho_CISEG0325/Aula 04-04/exercises/Ex8.py
@@ -2,19 +2,22 @@ print("Bem vindo")
 
 notas=[]
 
-for i in range (11):
+for i in range (10):
     nota=int(input(f"Digite a nota do aluno {i + 1} (0 a 20): "))
     notas.append(nota)
 
 media=sum(notas)/len(notas)
 
+notasacima=[]
+notasabaixo=[]
+
 for i in notas:
     if i>media:
-        j=i
+        notasacima.append(i)
     if i<media:
-        q=i
+        notasabaixo.append(i)
 
 
 print(f"A media das notas é: {media:.2f}")
-print(f"As seguintes notas ficaram acima: {j}")
-print(f"As seguintes notas ficaram abaixo: {q}")
+print(f"As seguintes notas ficaram acima: {notasacima}")
+print(f"As seguintes notas ficaram abaixo: {notasabaixo}")
