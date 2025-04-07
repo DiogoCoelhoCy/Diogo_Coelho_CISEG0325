1a9737cfa44236cdb44a81783705de3771fcc993
Diogo Coelho <diogo.coelho.t0130710@edu.atec.pt>
2025-04-07 16:19:21 +0100

    update 3.2.1

--- Diogo_Coelho_CISEG0325/Aula 04-04/exercises/LoopExercises/Ex6.py
+++ Diogo_Coelho_CISEG0325/Aula 04-04/exercises/LoopExercises/Ex6.py
@@ -9,7 +9,7 @@ while len(primos) < 10:
 
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            eh_primo = False
+            primo = False
             break
 
     if primo:
